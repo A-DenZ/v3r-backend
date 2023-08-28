18a5af4146c0b4033902f61434bd8f2e20d52e5c
Jay Carle Brodeur <95395907+A-DenZ@users.noreply.github.com>
2023-08-28 10:07:11 -0400

    First Model

---
+++ models/article.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const articleSchema = mongoose.Schema({
+    
+    title: String,
+
+    description: String,
+
+    link: {
+        name:String,
+        url:String
+    }
+    
+}, { timestamps: true })
+
+const ArticleModel = mongoose.model('ArticleModel', articleSchema , 'Articles' )
+
+export default ArticleModel
