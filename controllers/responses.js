9def80036522c6ab03920b0b5318167d5fe614e0
Jay Carle Brodeur <jaycontactpr00@gmail.com>
2023-10-23 10:55:13 -0400

    lots of changes

--- controllers/responses.js
+++ controllers/responses.js
@@ -1,7 +1,7 @@
-import { response } from "express"
-import { validateAudiSST } from "../utilities/request/request"
+import pool from "../config/db.config.js"
+import { validateAudiSST, validateForm , validateIncidentReport } from "../utilities/request/request"
 
-export const storeAudiResponse = async (req, res) => {
+export const storeAuditSSTResponse = async (req, res) => {
         try {
 
                 const { id,
@@ -57,6 +57,7 @@ export const storeAudiResponse = async (req, res) => {
                                                            )
                 const storeAuditSSTQuery = await pool.query(sqlInsertAuditSST, 
                                                                [
+                                                                id,
                                                                 incidentPlace,
                                                                 incidentDate,
                                                                 incidentHour,
@@ -77,7 +78,7 @@ export const storeAudiResponse = async (req, res) => {
                                                            )
                 
 
-                res.status(201).json(returnObject)
+                res.status(201).json(returnObject) // à voir si on fetch pas la bd à la place.
     
         }
         catch(error){
@@ -86,4 +87,105 @@ export const storeAudiResponse = async (req, res) => {
         }
     
    
-} 
+}
+
+
+export const storeIncidentReportResponse = async (req, res) => {
+        try{
+                const { id,
+                        userID,
+                        firstName,
+                        lastName,
+                        formName,
+
+                        unitsInvolved,
+                        departement,
+                        superior,
+                        driverLicense } = req.body
+                        
+                
+                
+                const storeValidForm = validateForm(id,userID,firstName,lastName,formName)
+                const storeValidIncidentReport = validateIncidentReport(unitsInvolved,departement,superior,driverLicense)         
+                
+                if(!storeValidForm?.[0]) return res.status(v?.[1]).json({message: v?.[2]})
+                if(!storeValidIncidentReport?.[0]) return res.status(v?.[1]).json({ message: v?.[2] })
+
+                const sqlInsertBaseForm = "INSERT INTO Form (id,userID,firstName,lastName,formName) VALUES (?,?,?,?,?)"
+                const sqlInsertIncidentReport = "INSERT INTO (formID,unitsInvolved,departement,superior,driverLicense) VALUES (?,?,?,?,?)"
+                const returnObject = {id,userID,firstName,lastName,formName,unitsInvolved,departement,superior,driverLicense}
+
+                const storeBaseFormQuery = await pool.query(sqlInsertBaseForm, [
+                                                                                id,
+                                                                                userID,
+                                                                                firstName,
+                                                                                lastName,
+                                                                                formName
+                                                                               ]
+                                                            )
+
+                const storeIncidentReport = await pool.query(sqlInsertIncidentReport, [
+                                                                                        id,
+                                                                                        unitsInvolved,
+                                                                                        departement,
+                                                                                        superior,
+                                                                                        driverLicense
+                                                                                      ]
+                                                            )
+
+
+                res.status(201).json(returnObject) // à voir si on fetch pas la bd à la place.
+
+        }
+        catch(error){
+                console.log(error)
+                res.status(500).json({message : 'Erreur du serveur'})
+        }
+}
+
+
+export const WorkingAccidentReport = async (req, res) =>{
+
+        try{
+
+                const { id,
+                        userID,
+                        firstName,
+                        lastName,
+                        formName,
+
+                        employeeCode,
+                        fonctionWhenHappend,
+                        accidentDate,
+                        accidentHour,
+                        witnesses,
+                        accidentPlace,
+                        activityCenter,
+                        injuries,
+                        injuriesDescription,
+                        physicalViolence,
+                        verbalViolence,
+                        accidentDescription,
+                        firstAid,
+                        secouristName,
+                        medicalConsultation,
+                        superiorIsAdvised,
+                        superior,
+                        superiorAdvisedOn,
+                        superiorSignature,
+                        superiorSignatureDate,
+                        superiorPostNum,
+                        workerSignature,
+                        workerSignatureDate,
+                        workerPostNum } = req.body
+
+                const storeValidForm = validateForm(id,userID,firstName,lastName,formName)
+                const storeValidWorkingAccidentReport = validateWorkingAccidentReport(employeeCode,fonctionWhenHappend,accidentDate,accidentHour,witnesses,accidentPlace,activityCenter,injuries,injuriesDescription,physicalViolence,verbalViolence,accidentDescription,firstAid,secouristName,medicalConsultation,superiorIsAdvised,superior,superiorAdvisedOn,superiorSignature,superiorSignatureDate,superiorPostNum,workerSignature,workerSignatureDate,workerPostNum)
+
+        }
+        catch(error){
+                console.log(error)
+                res.status(500).json({message : 'Erreur du serveur'})
+        }
+
+}
--- utilities/regexp/regexp.js
+++ utilities/regexp/regexp.js
@@ -1,2 +1,6 @@
-export const defaultInputFilter = new RegExp(/^[\t\r\n]|(--[^\r\n]*)|(\/\*[\w\W]*?(?=\*)\*\$/)
-export const numberFilter = new RegExp(/^[0-9]*$/)
+export const defaultInputFilter = new RegExp(/^[\t\r\n]|(--[^\r\n]*)|(\/\*[\w\W]*?(?=\*)\*\/)/);
+export const numberFilter = new RegExp(/^[0-9]*$/);
+export const driverLicenseFilter = new RegExp(/^[A-Z]\d{4}-\d{6}-\d{2}$/);
+export const dateFilter = new RegExp(/^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/)
+
+
