b2928d1284cbfd3a38f8fd3b9ba51c49c9bf9b94
Jay Carle Brodeur <jaycontactpr00@gmail.com>
2023-10-30 11:35:01 -0400

    Fixed lot of problems AuditSST and IncidentReport  are working

--- controllers/responses.js
+++ controllers/responses.js
@@ -1,25 +1,18 @@
 
 import pool from "../config/db.config.js"
+import { v4 as uuidv4 } from 'uuid';
 import { validateAudiSST, validateForm , validateIncidentReport , validateWorkingAccidentReport , validateSSD } from "../utilities/request/request.js"
 import { sendMailNotification } from "../utilities/mailjet/mailjet.js"
 
 
 export const storeAuditSSTResponse = async (req, res) => {
         try {
-
-                const { id,
+                const uniqueID = uuidv4();
+                const { 
                         userID,
                         firstName,
                         lastName,
                         formName,
-                        creationDate, // BOF
-                        modificationDate, // BOF
-                        readOn, // BOF
-                        isRead, // BOF
-                        readBy, // BOF
-                        isOpen, // BOF
-                        openedBy, // BOF
-                        openedOn, // BOF
 
                         incidentPlace,
                         incidentDate,
@@ -37,31 +30,34 @@ export const storeAuditSSTResponse = async (req, res) => {
                         EPIAreOn,
                         procedureRespect,
                         incidentDescription,
-
                         superior } = req.body
                 
-                const storeValidForm = validateForm(id,userID,firstName,lastName,formName)
+                const storeValidForm = validateForm(userID,firstName,lastName,formName)
                 const storeValidAuditSST = validateAudiSST(incidentPlace, incidentDate, incidentHour, EPI, placeConform, safeComportement, signalytics, signalyticsSheets, workingExcavation, confinedSpace, workingMethod, others, distanceRespected, EPIAreOn, procedureRespect, incidentDescription) // returns array conataining [boolean, status, message]
 
-                if(!storeValidForm?.[0]) return res.status(v?.[1]).json({message: v?.[2]})
-                if(!storeValidAuditSST?.[0]) return res.status(v?.[1]).json({ message: v?.[2] })
+                if(!storeValidForm?.[0]) return res.status(storeValidForm?.[1]).json({message: storeValidForm?.[2]})
+                if(!storeValidAuditSST?.[0]) return res.status(storeValidAuditSST?.[1]).json({ message: storeValidAuditSST?.[2] })
                 
-                const returnObject = {id,userID,firstName,lastName,formName,incidentPlace, incidentDate, incidentHour, EPI, placeConform, safeComportement, signalytics, signalyticsSheets, workingExcavation, confinedSpace, workingMethod, others, distanceRespected, EPIAreOn, procedureRespect, incidentDescription}
-                const sqlInsertBaseForm = "INSERT INTO Form (id,userID,firstName,lastName,formName) VALUES (?,?,?,?,?)"
-                const sqlInsertAuditSST = "INSERT INTO AuditSST (formID, incidentPlace, incidentDate, incidentHour, EPI, placeConform, safeComportement, signalytics, signalyticsSheets, workingExcavation, confinedSpace, workingMethod, others, distanceRespected, EPIAreOn, procedureRespect, incidentDescription) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
-                
+                //const returnObject = {uniqueID,userID,firstName,lastName,formName,incidentPlace, incidentDate, incidentHour, EPI, placeConform, safeComportement, signalytics, signalyticsSheets, workingExcavation, confinedSpace, workingMethod, others, distanceRespected, EPIAreOn, procedureRespect, incidentDescription}
+                const sqlInsertBaseForm = "INSERT INTO Form (userID,firstName,lastName,formName) VALUES (?,?,?,?)"
+                const sqlFetchForm = "SELECT id FROM form WHERE userID = ? AND firstName = ? AND lastName = ? AND formName = ? ORDER BY id DESC LIMIT 1"
+                const sqlInsertAuditSST = "INSERT INTO AuditSST (formID, incidentPlace, incidentDate, incidentHour, EPI, placeConform, safeComportement, signalytics, signalyticsSheets, workingExcavation, confinedSpace, workingMethod, others, distanceRespected, EPIAreOn, procedureRespect, incidentDescription) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+                const sqlFetchAllInfos = "SELECT * FROM Form JOIN AuditSST ON Form.id = AuditSST.formID WHERE Form.id = ? LIMIT 1"
                 
                 const storeBaseFormQuery = await pool.query(sqlInsertBaseForm, [
-                                                                                id,
                                                                                 userID,
                                                                                 firstName,
                                                                                 lastName,
                                                                                 formName
                                                                                ]
                                                            )
+
+                const formIDFetch = await pool.query(sqlFetchForm, [userID, firstName, lastName, formName])
+                let formID = formIDFetch[0]?.[0]?.id
+
                 const storeAuditSSTQuery = await pool.query(sqlInsertAuditSST, 
                                                                [
-                                                                id,
+                                                                formID,
                                                                 incidentPlace,
                                                                 incidentDate,
                                                                 incidentHour,
@@ -80,9 +76,11 @@ export const storeAuditSSTResponse = async (req, res) => {
                                                                 incidentDescription
                                                                ]
                                                            )
-                
 
-                res.status(201).json(returnObject) // à voir si on fetch pas la bd à la place.
+                const returnFormObject = await pool.query(sqlFetchAllInfos,formID)
+
+                
+                res.status(201).json(returnFormObject[0][0]) // à voir si on fetch pas la bd à la place.
 
 
                    const superiorName = superior
@@ -108,8 +106,7 @@ export const storeAuditSSTResponse = async (req, res) => {
 
 export const storeIncidentReportResponse = async (req, res) => {
         try{
-                const {
-                        userID,
+                const { userID,
                         firstName,
                         lastName,
                         formName,
@@ -121,18 +118,20 @@ export const storeIncidentReportResponse = async (req, res) => {
                         
                 
                 
-                const storeValidForm = validateForm(id,userID,firstName,lastName,formName)
+                const storeValidForm = validateForm(userID,firstName,lastName,formName)
                 const storeValidIncidentReport = validateIncidentReport(unitsInvolved,departement,superior,driverLicense)         
                 
-                if(!storeValidForm?.[0]) return res.status(v?.[1]).json({message: v?.[2]})
-                if(!storeValidIncidentReport?.[0]) return res.status(v?.[1]).json({ message: v?.[2] })
+                if(!storeValidForm?.[0]) return res.status(storeValidForm?.[1]).json({message: storeValidForm?.[2]})
+                if(!storeValidIncidentReport?.[0]) return res.status(storeValidForm?.[1]).json({ message: storeValidForm?.[2] })
 
-                const sqlInsertBaseForm = "INSERT INTO Form (id,userID,firstName,lastName,formName) VALUES (?,?,?,?,?)"
+                const sqlInsertBaseForm = "INSERT INTO Form (userID,firstName,lastName,formName) VALUES (?,?,?,?)"
                 const sqlInsertIncidentReport = "INSERT INTO IncidentReport (formID,unitsInvolved,departement,superior,driverLicense) VALUES (?,?,?,?,?)"
-                const returnObject = {id,userID,firstName,lastName,formName,unitsInvolved,departement,superior,driverLicense}
+                const sqlFetchForm = "SELECT id FROM form WHERE userID = ? AND firstName = ? AND lastName = ? AND formName = ? ORDER BY id DESC LIMIT 1"
+                const sqlFetchAllInfos = "SELECT * FROM Form JOIN IncidentReport ON Form.id = IncidentReport.formID WHERE Form.id = ? LIMIT 1"
+                
+                
 
                 const storeBaseFormQuery = await pool.query(sqlInsertBaseForm, [
-                                                                                id,
                                                                                 userID,
                                                                                 firstName,
                                                                                 lastName,
@@ -140,8 +139,12 @@ export const storeIncidentReportResponse = async (req, res) => {
                                                                                ]
                                                             )
 
+                const formIDFetch = await pool.query(sqlFetchForm, [userID, firstName, lastName, formName])
+
+                let formID = formIDFetch[0]?.[0]?.id
+                
                 const storeIncidentReport = await pool.query(sqlInsertIncidentReport, [
-                                                                                        id,
+                                                                                        formID,
                                                                                         unitsInvolved,
                                                                                         departement,
                                                                                         superior,
@@ -149,8 +152,10 @@ export const storeIncidentReportResponse = async (req, res) => {
                                                                                       ]
                                                             )
 
+                const returnFormObject = await pool.query(sqlFetchAllInfos,formID)
 
-                res.status(201).json(returnObject) // à voir si on fetch pas la bd à la place.
+
+                res.status(201).json(returnFormObject[0][0])
 
         }
         catch(error){
@@ -163,9 +168,8 @@ export const storeIncidentReportResponse = async (req, res) => {
 export const storeWorkingAccidentReport = async (req, res) => {
 
         try{
-
-                const { id,
-                        userID,
+                const uniqueID = uuidv4();
+                const { userID,
                         firstName,
                         lastName,
                         formName,
@@ -191,7 +195,7 @@ export const storeWorkingAccidentReport = async (req, res) => {
                         superiorPostNum,
                         workerPostNum } = req.body
 
-                const storeValidForm = validateForm(id,userID,firstName,lastName,formName)
+                const storeValidForm = validateForm(uniqueID,userID,firstName,lastName,formName)
                 const storeValidWorkingAccidentReport = validateWorkingAccidentReport(employeeCode,fonctionWhenHappend,accidentDate,accidentHour,witnesses,accidentPlace,activityCenter,injuries,injuriesDescription,physicalViolence,verbalViolence,accidentDescription,firstAid,secouristName,medicalConsultation,superiorIsAdvised,superior,superiorAdvisedOn,superiorPostNum,workerPostNum)
                 
 
@@ -201,10 +205,10 @@ export const storeWorkingAccidentReport = async (req, res) => {
 
                 const sqlInsertBaseForm = "INSERT INTO Form (id,userID,firstName,lastName,formName) VALUES (?,?,?,?,?)"
                 const sqlInsertWorkingAccidentReport = "INSERT INTO WorkingAccidentReport (formID,employeeCode,fonctionWhenHappend,accidentDate,accidentHour,witnesses,accidentPlace,activityCenter,injuries,injuriesDescription,physicalViolence,verbalViolence,accidentDescription,firstAid,secouristName,medicalConsultation,superiorIsAdvised,superior,superiorAdvisedOn,superiorPostNum,workerPostNum) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)" 
-                const returnObject = {id,userID,firstName,lastName,formName,employeeCode,fonctionWhenHappend,accidentDate,accidentHour,witnesses,accidentPlace,activityCenter,injuries,injuriesDescription,physicalViolence,verbalViolence,accidentDescription,firstAid,secouristName,medicalConsultation,superiorIsAdvised,superior,superiorAdvisedOn,superiorPostNum,workerPostNum}
+                const returnObject = {uniqueID,userID,firstName,lastName,formName,employeeCode,fonctionWhenHappend,accidentDate,accidentHour,witnesses,accidentPlace,activityCenter,injuries,injuriesDescription,physicalViolence,verbalViolence,accidentDescription,firstAid,secouristName,medicalConsultation,superiorIsAdvised,superior,superiorAdvisedOn,superiorPostNum,workerPostNum}
 
                 const storeBaseFormQuery = await pool.query(sqlInsertBaseForm, [
-                                                                                id,
+                                                                                uniqueID,
                                                                                 userID,
                                                                                 firstName,
                                                                                 lastName,
@@ -249,10 +253,8 @@ export const storeWorkingAccidentReport = async (req, res) => {
 
 export const storeSSD = async (req,res) => {
         try{
-
-                const {
-                        id,
-                        userID,
+                const uniqueID = uuidv4();
+                const { userID,
                         firstName,
                         lastName,
                         formName,
@@ -270,10 +272,10 @@ export const storeSSD = async (req,res) => {
                         superiorAdvisedOn,
                         superiorPostNum} = req.body
                 
-                const storeValidForm = validateForm(id,userID,firstName,lastName,formName)
+                const storeValidForm = validateForm(uniqueID,userID,firstName,lastName,formName)
                 const storeValidSSD  = validateSSD(employeeCode,fonctionWhenHappend,activityCenter,incidentDate,incidentHour,witnesses,incidentDescription,correctionsOrAddOn,superiorIsAdvised,superior,superiorAdvisedOn,superiorPostNum)
                 
-                const returnObject = {id,userID,firstName,lastName,formName,employeeCode,fonctionWhenHappend,activityCenter,incidentDate,incidentHour,witnesses,incidentDescription,correctionsOrAddOn,superiorIsAdvised,superior,superiorAdvisedOn,superiorPostNum}
+                const returnObject = {uniqueID,userID,firstName,lastName,formName,employeeCode,fonctionWhenHappend,activityCenter,incidentDate,incidentHour,witnesses,incidentDescription,correctionsOrAddOn,superiorIsAdvised,superior,superiorAdvisedOn,superiorPostNum}
 
                 if(!storeValidForm?.[0]) return res.status(v?.[1]).json({message: v?.[2]})
                 if(!storeValidSSD?.[0]) return res.status(v?.[1]).json({message: v?.[2]})
@@ -283,7 +285,7 @@ export const storeSSD = async (req,res) => {
                 
 
                 const storeBaseFormQuery = await pool.query(sqlInsertBaseForm, [
-                                                                                id,
+                                                                                uniqueID,
                                                                                 userID,
                                                                                 firstName,
                                                                                 lastName,
@@ -291,6 +293,7 @@ export const storeSSD = async (req,res) => {
                                                                                 ]
                                                            )
                 const storeSDDQuery = await pool.query(sqlInsertSSD, [
+                                                                        uniqueID,
                                                                         employeeCode,
                                                                         fonctionWhenHappend,
                                                                         activityCenter,
--- utilities/request/request.js
+++ utilities/request/request.js
@@ -1,33 +1,34 @@
 import { defaultInputFilter , numberFilter, driverLicenseFilter, dateFilter, hourFilter } from "../regexp/regexp.js"
 
-export const validateForm = (id,userID,firstName,lastName,formName) => {
+export const validateForm = (userID,firstName,lastName,formName) => {
     let isValid = true
     let validStatus = 200
     let invalidStatus = 400
     let okMessage = "Le formulaire à été correctement validé.";
     let blockMessage = "Une erreur de saisie bloque l'envoi du formulaire."
 
-    if(!numberFilter.test(id)){ // à ajuster peux-être
-        isValid = false
-    }
 
-    if(!numberFilter.test(userID)){ // à ajuster peux-être
+    if(!numberFilter.test(userID) || userID == "" || userID == null){ // à ajuster peux-être
         isValid = false
+        console.log("userID")
     }
 
     // PROTECTION CONTRE L'INJECTION SQL ET L'INJECTION DE SCRIPT
 
     if(defaultInputFilter.test(firstName) || firstName == "" || firstName == null){
         isValid = false
+        console.log("first")
     }
 
     if(defaultInputFilter.test(lastName) || lastName == "" || lastName == null ){
         isValid = false
+        console.log("last")
     }
 
-    if(formName !== "SSD" || formName !== "AuditSST" || formName !== "WorkingAccidentReport" || formName !== "IncidentReport"){
-        isValid = false
-    }
+    if (formName !== "SSD" && formName !== "AuditSST" && formName !== "WorkingAccidentReport" && formName !== "IncidentReport") {
+        isValid = false;
+        console.log("formName");
+    }    
     
     if(isValid) {
         return [true, validStatus , okMessage]
@@ -51,65 +52,81 @@ export const validateAudiSST = (incidentPlace, incidentDate, incidentHour, EPI, 
 
 
 
-    if (incidentPlace !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    if (defaultInputFilter.test(incidentPlace) || incidentPlace == ""  || incidentPlace == null) {
         isValid = false;
+        console.log("1" , incidentPlace)
     }
 
-    if (incidentDate !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    if (!dateFilter.test(incidentDate) || incidentDate == "" || incidentDate == null) {
         isValid = false;
+        console.log("2" , incidentDate)
     }
 
-    if (incidentHour !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    if (!hourFilter.test(incidentHour) || incidentHour == "" || incidentHour == null) {
         isValid = false;
+        console.log("3" , incidentHour)
     }
-
-    if (EPI !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(EPI === "Conforme" || EPI === "Non conforme" || EPI === "N/A" || EPI === "" || EPI === null)) {
         isValid = false;
+        console.log("4", EPI);
     }
-
-    if (placeConform !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(placeConform === "Conforme" || placeConform === "Non conforme" || placeConform === "N/A" || placeConform === "" || placeConform === null)) {
         isValid = false;
+        console.log("5", placeConform);
     }
-
-    if (safeComportement !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(safeComportement === "Conforme" || safeComportement === "Non conforme" || safeComportement === "N/A" || safeComportement === "" || safeComportement === null)) {
         isValid = false;
+        console.log("6", safeComportement);
     }
-
-    if (signalytics !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(signalytics === "Conforme" || signalytics === "Non conforme" || signalytics === "N/A" || signalytics === "" || signalytics === null)) {
         isValid = false;
+        console.log("7", signalytics);
     }
-
-    if (signalyticsSheets !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(signalyticsSheets === "Conforme" || signalyticsSheets === "Non conforme" || signalyticsSheets === "N/A" || signalyticsSheets === "" || signalyticsSheets === null)) {
         isValid = false;
+        console.log("8", signalyticsSheets);
     }
-
-    if (workingExcavation !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(workingExcavation === "Conforme" || workingExcavation === "Non conforme" || workingExcavation === "N/A" || workingExcavation === "" || workingExcavation === null)) {
         isValid = false;
+        console.log("9", workingExcavation);
     }
-
-    if (confinedSpace !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(confinedSpace === "Conforme" || confinedSpace === "Non conforme" || confinedSpace === "N/A" || confinedSpace === "" || confinedSpace === null)) {
         isValid = false;
+        console.log("10", confinedSpace);
     }
-
-    if (workingMethod !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(workingMethod === "Conforme" || workingMethod === "Non conforme" || workingMethod === "N/A" || workingMethod === "" || workingMethod === null)) {
         isValid = false;
+        console.log("11", workingMethod);
     }
-
-    if (distanceRespected !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(distanceRespected === "Conforme" || distanceRespected === "Non conforme" || distanceRespected === "N/A" || distanceRespected === "" || distanceRespected === null)) {
         isValid = false;
+        console.log("12", distanceRespected);
     }
-
-    if (EPIAreOn !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(EPIAreOn === "Conforme" || EPIAreOn === "Non conforme" || EPIAreOn === "N/A" || EPIAreOn === "" || EPIAreOn === null)) {
         isValid = false;
+        console.log("13", EPIAreOn);
     }
-
-    if (procedureRespect !== "Conforme" || incidentPlace !== "Non conforme" || incidentPlace !== "N/A") {
+    
+    if (!(procedureRespect === "Conforme" || procedureRespect === "Non conforme" || procedureRespect === "N/A" || procedureRespect === "" || procedureRespect === null)) {
         isValid = false;
+        console.log("14", procedureRespect);
     }
+    
 
     // PROTECTION CONTRE L'INJECTION SQL ET L'INJECTION DE SCRIPT
     if(defaultInputFilter.test(incidentDescription) || incidentDescription == "" || incidentDescription == null) {
         isValid = false;
+        console.log("15" , incidentDescription)
     } 
 
 
