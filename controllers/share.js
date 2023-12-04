3147998462efffe606a3d62d6500a1aa31f83cb6
Furi3NTM <charles.varindupuis@hotmail.fr>
2023-12-04 08:35:54 -0500

    add real data in shareinfo database

---
+++ controllers/share.js
@@ -0,0 +1,43 @@
+import pool from "../config/db.config.js"
+import dotenv from 'dotenv'
+import {validateForm} from '../utilities/request/request.js'
+
+export const storeSharedLink = async (req, res) => {
+    try{
+            const {
+                id,
+                title,
+                link,
+            } = req.body
+
+            console.log(req.body)
+
+       // const storeValidInformation = validateForm(title,link)
+       // if(!storeValidInformation?.[0]) return res.status(storeValidInformation?.[1]).json({message: storeValidInformation?.[2]})
+        
+       const sqlInsertSharedLink = "INSERT INTO ShareInfo (id,title,link) VALUES (?,?,?)"
+       const sqlFetchAllInfos = "SELECT * FROM ShareInfo"
+
+       const storeSharedLinkQuery = await pool.query(sqlInsertSharedLink, [id, title,link])
+       const returnFormObject = await pool.query(sqlFetchAllInfos)
+
+    } catch(error){
+        console.log(error)
+        res.status(500).json({message : 'Erreur du serveur'})
+}
+}
+
+export const fetchAllSharedLink = async (req,res) => {
+
+    try{
+
+        const sqlFetchAllSharedLink = "SELECT * FROM ShareInfo"
+        const storeAllSharedLink = await pool.query(sqlFetchAllSharedLink)
+
+        console.log(storeAllSharedLink)
+        res.status(201).json(storeAllSharedLink)
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message : 'Erreur du serveur'})
+}
+}
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.routes.js';
-import responseRoutes from './routes/response.routes.js';
-
+import dotenv from 'dotenv'
+import userRoutes from './routes/user.routes.js'
+import responseRoutes from './routes/response.routes.js'
+import shareRoutes from './routes/share.routes.js'
 
 dotenv.config();
 
@@ -16,8 +16,9 @@ app.use(express.json());
 app.use(cors());
 
 
-app.use('/user' , userRoutes)
-app.use('/response', responseRoutes)
+app.use('/user' , userRoutes);
+app.use('/response', responseRoutes);
+app.use('/share', shareRoutes);
 
 
 app.use((err , req , res , next) => {
