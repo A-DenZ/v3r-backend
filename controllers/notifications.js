ac05717f61257087b8e27e490ee3ddcf7147792d
Jay Carle Brodeur <95395907+A-DenZ@users.noreply.github.com>
2023-12-18 15:36:42 -0500

    notif working for now

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -26,9 +26,6 @@ export const updateOpenedOn = async (req,res) => {
 
         
         const storeTheNotif = await pool.query(sqlFetchTheNotif, id)
-
-
-                                                                
         
         res.status(201).json(storeTheNotif[0])
 
@@ -39,34 +36,14 @@ export const updateOpenedOn = async (req,res) => {
 }
 
 
-export const fetchAllNotification = async (req,res) => {
-
-    try{
-
-        const sqlFetchAllNotif = "SELECT * FROM Notification"
-
-        const storeAllNotif = await pool.query(sqlFetchAllNotif)
-
-
-        res.status(201).json(storeAllNotif[0])
-
-    }catch(error){
-
-    }
-}
-
-
 export const fetchAllNotificationByUser = async (req,res) => {
     try{
         
-        const {
-            userID
-        } = req.body
 
-        const sqlFetchAllNotifByUser = "SELECT * FROM Notification WHERE targetedUser = ?"
-        const sqlAllNotifGoodInfoQuery = `SELECT n.id AS NotificationID, CONCAT(tb.firstName, ' ', tb.lastName) AS TriggeredByName, CONCAT(tu.firstName, ' ', tu.lastName) AS TargetedUserName, CONCAT(nf.firstName, ' ', nf.lastName) AS ForUser, n.type, n.formID, n.isOpened, n.openedOn, n.createdDate FROM Notification AS n INNER JOIN User AS tb ON n.triggeredBy = tb.id INNER JOIN User AS tu ON n.targetedUser = tu.id LEFT JOIN Form AS nf ON n.formID = nf.id;`;
-        const storeAllNotifByUserQuery = await pool.query(sqlFetchAllNotifByUser, userID)
-        const storeAllGoodInfoQuery = await pool.query(sqlAllNotifGoodInfoQuery)
+        const { userID } = req.params
+
+        const sqlAllNotifGoodInfoQuery = `SELECT n.id AS NotificationID, CONCAT(tb.firstName, ' ', tb.lastName) AS TriggeredByName, CONCAT(tu.firstName, ' ', tu.lastName) AS TargetedUserName, CONCAT(nf.firstName, ' ', nf.lastName) AS ForUser, n.type, n.formID, n.isOpened FROM Notification AS n INNER JOIN User AS tb ON n.triggeredBy = tb.id INNER JOIN User AS tu ON n.targetedUser = tu.id LEFT JOIN Form AS nf ON n.formID = nf.id WHERE tb.id = ?;`;
+        const storeAllGoodInfoQuery = await pool.query(sqlAllNotifGoodInfoQuery , userID)
         
         res.status(201).json(storeAllGoodInfoQuery[0])
 
--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { updateOpenedOn, fetchAllNotification, fetchAllNotificationByUser } from '../controllers/notifications.js';
+import { updateOpenedOn, fetchAllNotificationByUser } from '../controllers/notifications.js';
 
 const router = express.Router();
 
-router.get('/fetchall' , fetchAllNotification ); 
-router.get('/fetch' , fetchAllNotificationByUser );
+router.get('/fetch/:userID' ,fetchAllNotificationByUser );
 router.put('/opened' , updateOpenedOn)
 
 export default router;
